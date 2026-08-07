a4ac32343dca09b288d083cf77b2686d0f3656b3
Gurukumar <gurushetty018@gmail.com>
2026-08-07 12:13:47 +0530

    updated apln properties'

--- src/main/java/com/Employee/employHub/service/UserService.java
+++ src/main/java/com/Employee/employHub/service/UserService.java
@@ -16,63 +16,39 @@ import com.Employee.employHub.util.OtpGenerator;
 public class UserService {
 
 	private final UserRepository repository;
-	private final JavaMailSender javaMailSender;
-	
-	
-	
+	private final EmailService emailService;
 
-	
-
-	public UserService(UserRepository repository, JavaMailSender javaMailSender) {
+	public UserService(UserRepository repository, EmailService emailService) {
 		this.repository = repository;
-		this.javaMailSender = javaMailSender;
+		this.emailService = emailService;
 	}
 
-
-	//user registration
+	// user registration
+	// UserService.java
 	public Object Registration(RegisterRequest request) {
 		Optional<User> op = repository.findByEmail(request.getEmail());
 		if (op.isPresent()) {
-			return"email is already registred";
-			
-		}
-		else {
+			return "email is already registred";
+		} else {
 			User user = new User();
 			user.setName(request.getName());
 			user.setEmail(request.getEmail());
 			user.setPassword(request.getPassword());
 			user.setRole("user");
+
 			String otp = OtpGenerator.otpGenerate();
 			user.setOtp(otp);
-			
 			user.setOtpExpire(LocalDateTime.now().plusMinutes(5));
-			
 			user.setVarify(false);
-			return "otp sent to "+request.getEmail();
-		}
-		
-		
-		
-		
-	}
-	
-	
-	 // OTP generator
-    private String otpGeneration() {
-        Random random = new Random();
-        return String.format("%06d", random.nextInt(999999));
-    }
 
-    // Send OTP email
-    private void sendMail(String toEmail, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Verification OTP");
-        message.setText("Your OTP is: " + otp + "\nIt is valid for 5 minutes.");
-        javaMailSender.send(message);
-    }
-    
-   
-	
-	
+			// --- MISSING STEP: SAVE TO DATABASE ---
+			repository.save(user);
+
+			// Send Email
+			emailService.SendMail(user.getEmail(), otp);
+
+			return "otp sent to " + request.getEmail();
+		}
+	}
+
 }
